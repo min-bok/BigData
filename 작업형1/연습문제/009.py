1c0ded9973ae9c3f9f168b374fd0897d883277a8
min-bok <leehy0782@gmail.com>
2025-04-25 22:08:41 +0900

    250425 commit

--- 작업형1/연습문제/009.py
+++ 작업형1/연습문제/009.py
@@ -1,2 +1,17 @@
 # 1. views 컬럼 값이 1000 이하인 데이터(결측치 제외)를 찾으시오
 # 2. 앞에서 구한 데이터 중 f4 컬럼의 최빈값을 구하시오
+import pandas as pd
+
+df = pd.read_csv("./type1_data1.csv")
+# print(df.head())
+
+# print(df.isna().sum())
+df = df.dropna(subset=["views"])
+# print(df.isna().sum())
+
+cond1 = (df["views"] <= 1000)
+new = df[cond1]
+# print(df["views"].sort_values())
+
+# print(new["f4"].mode()[0])
+print(new["f4"].value_counts().index[0])
--- 작업형1/연습문제/010.py
+++ 작업형1/연습문제/010.py
@@ -1,1 +1,19 @@
-# 1. 
+# 1. 결측치가 있는 행을 삭제하시오
+# 2. 결측치가 삭제된 제이터를 사용하여 지역별(city) 평균을 계산하시오
+# 3. 앞에서 계산한 지역별 평균 데이터에서 f2 컬럼 값이 가장 큰 지역을 구하시오
+import pandas as pd
+
+df = pd.read_csv("./type1_data1.csv")
+# print(df.head())
+
+# print(df.isna().sum())
+df = df.dropna()
+# print(df.isna().sum())
+
+# print(df.head(10))
+
+# print(df.groupby(["city"]).mean(numeric_only=True))
+df = df.groupby(["city"]).mean(numeric_only=True)
+# print(df.head())
+
+print(df.sort_values("f2", ascending=False).index[0])
