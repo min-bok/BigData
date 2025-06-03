3e80eab528696344c67aeb3fdb5e3fad1a644f2f
min-bok <leehy0782@gmail.com>
2025-06-03 23:04:30 +0900

    250603 작업형1 commit

--- 작업형1/연습문제2/006⭐.py
+++ 작업형1/연습문제2/006⭐.py
@@ -10,6 +10,6 @@ df = pd.read_csv("./type1_data1.csv")
 cond1 = df["f3"] == "gold"
 cond2 = df["f2"] == 2
 
-temp = df[(cond1) & (cond2)] 
+temp = df[(cond1) & (cond2)] # ⭐
 
-print(round(temp["f1"].var(),2))
+print(round(temp["f1"].var(),2)) # ⭐
---
+++ 작업형1/연습문제2/018⭐.py
@@ -0,0 +1,12 @@
+# 1. index "2001" 데이터(행)에서 평균보다 큰 값의 개수를 구하시오
+# 2. index "2003" 데이터(행)에서 평균보다 작은 값의 개수를 구하시오
+# 3. 두 개수를 더하시오
+import pandas as pd
+
+df = pd.read_csv("./type1_data2.csv", index_col="year") # ⭐ 특정 컬럼을 데이터프레임의 인덱스로 설정하는 옵션
+# print(df.head())
+
+cond1 = df.loc[2001] > df.loc[2001].mean() # ⭐
+cond2 = df.loc[2003] < df.loc[2003].mean() # ⭐
+
+print(cond1.sum() + cond2.sum()) # 202
